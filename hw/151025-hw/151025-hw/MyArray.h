bb272305795e2ec3cdf9617b1f66d618038047bf
dimtha <143012267+dimtha@users.noreply.github.com>
2025-11-23 19:21:48 +0300

    Fix loop condition in MyArray copy constructor

--- hw/151025-hw/151025-hw/MyArray.h
+++ hw/151025-hw/151025-hw/MyArray.h
@@ -17,7 +17,7 @@ public:
 	MyArray(const MyArray& obj) {
 		this->size = obj.size;
 		this->array = new int[size+1];
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < this->size; i++) {
 			this->array[i] = obj.array[i];
 		}
 	}
